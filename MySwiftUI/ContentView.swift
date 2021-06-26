9b9b71f39633ecb7ed6e48fd7d3255d73bea64d6
sd <dhayal.cluzter@gmail.com>
2021-06-26 11:45:06 +0530

    Initial Commit

---
+++ MySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUI
+//
+//  Created by dhayalprema on 26/06/21.
+//  Copyright © 2021 dhayalprema. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
